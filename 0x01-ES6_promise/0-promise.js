78e06fc99a1cdc4dd60fa2e92eadcc7cdb85138c
IsaacPhiri <isaacphiri315@gmail.com>
2023-03-23 23:35:38 +0000

    edits

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
-function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-    	// perform some asynchronous operation here
-    	// and call resolve() with the result if successful
-    	// or reject() with an error message
-	// if there is an error
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
