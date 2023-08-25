acc427f1d6fcd189eb0a5913c372cc4d1161a100
Isaac Phiri <isaacphiri315@gmail.com>
2023-08-25 13:56:52 +0200

    Deep equality & Post integration testing.

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,37 @@
+// Description: API server
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for ${id}`);
+});
+
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
+});
+
+app.post('/login', (req, res) => {
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome :${username}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`API running on port ${PORT}`);
+});
+
+module.exports = app;
